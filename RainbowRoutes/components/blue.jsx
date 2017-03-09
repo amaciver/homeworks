c28ca1e6504409e3776cb878b5f3c28e02dbdc1a
amaciver <maciver.andrew@gmail.com>
2017-03-09 08:40:22 -0800

    Add files via upload

---
+++ RainbowRoutes/components/blue.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { withRouter } from 'react-router';
+
+class Blue extends React.Component {
+  constructor() {
+    super();
+    this.resetBlue = this.resetBlue.bind(this);
+    this.addIndigo = this.addIndigo.bind(this);
+  }
+
+  render() {
+    return(
+      <div>
+        <h2 className="blue"></h2>
+        <h4 onClick={this.resetBlue}>Blue only</h4>
+        <h4 onClick={this.addIndigo}>Add indigo</h4>
+
+        {this.props.children}
+      </div>
+    );
+  }
+
+  resetBlue() {
+    this.props.router.push('/blue');
+    // your code here
+  }
+
+  addIndigo() {
+    this.props.router.push('/blue/indigo');
+    // your code here
+  }
+};
+
+export default withRouter(Blue);
---
+++ RainbowRoutes/components/red.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { withRouter } from 'react-router';
+
+class Red extends React.Component {
+  constructor() {
+    super();
+    this.resetRed = this.resetRed.bind(this);
+    this.addOrange = this.addOrange.bind(this);
+    this.addYellow = this.addYellow.bind(this);
+  }
+
+  render() {
+    return(
+      <div>
+        <h2 className="red"></h2>
+        <h4 onClick={this.resetRed}>Red only</h4>
+        <h4 onClick={this.addOrange}>Add orange</h4>
+        <h4 onClick={this.addYellow}>Add yellow</h4>
+
+        {this.props.children}
+      </div>
+    );
+  }
+
+  resetRed() {
+    this.props.router.push('/red');
+    // your code here
+  }
+
+  addOrange() {
+    this.props.router.push('/red/orange');
+    // your code here
+  }
+
+  addYellow() {
+    this.props.router.push('/red/yellow');
+    // your code here
+  }
+};
+
+export default withRouter(Red);
